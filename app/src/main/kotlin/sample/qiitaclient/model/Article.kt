6e937e459058c39847ff5e1d46664b1d6fe75d4f
honda kouichi <honda.kouichi@actindi.net>
2016-08-09 02:22:49 +0900

    15章-1

---
+++ app/src/main/kotlin/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,10 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by kouichihonda on 2016/08/09.
+ * Copyright © 2016年 actindi Inc. All rights reserved.
+ */
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ app/src/main/kotlin/sample/qiitaclient/model/User.kt
@@ -0,0 +1,9 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by kouichihonda on 2016/08/09.
+ * Copyright © 2016年 actindi Inc. All rights reserved.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
