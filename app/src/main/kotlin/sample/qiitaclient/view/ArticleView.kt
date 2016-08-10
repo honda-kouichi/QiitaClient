6599378ce97fe5e6171caa1f6b575a17add1b6ef
honda kouichi <honda.kouichi@actindi.net>
2016-08-11 02:21:05 +0900

    15.7 15.8

--- app/src/main/kotlin/sample/qiitaclient/view/ArticleView.kt
+++ app/src/main/kotlin/sample/qiitaclient/view/ArticleView.kt
@@ -8,10 +8,9 @@ import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import org.w3c.dom.Text
 import sample.qiitaclient.R
+import sample.qiitaclient.bindView
 import sample.qiitaclient.model.Article
-import java.util.jar.Attributes
 
 /**
  * Created by kouichihonda on 2016/08/09.
@@ -28,16 +27,10 @@ class ArticleView: FrameLayout {
                 attrs: AttributeSet?,
                 defStyleAttr: Int,
                 defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
-    
-    val profileImageView: ImageView by lazy {
-        findViewById(R.id.profile_image_view) as ImageView
-    }
-    val titleTextView: TextView by lazy {
-        findViewById(R.id.title_text_view) as TextView
-    }
-    val userNameTextView: TextView by lazy {
-        findViewById(R.id.user_name_text_view) as TextView
-    }
+
+    val profileImageView: ImageView by bindView<ImageView>(R.id.profile_image_view)
+    val titleTextView: TextView by bindView<TextView>(R.id.title_text_view)
+    val userNameTextView: TextView by bindView<TextView>(R.id.user_name_text_view)
 
     init {
         LayoutInflater.from(context).inflate(R.layout.view_article, this)
